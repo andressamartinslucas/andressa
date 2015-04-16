c88fbef90f979edccb61c6557c505143cb3322e0
Andressa M. Lucas <andressamartinslucas@gmail.com>
2015-04-16 11:13:20 -0300

    Revert "Revert """
    This reverts commit b9707a3ccdcff447e7fa0e58756b01ced9c8b283.

---
+++ showdoMilhaoAndressa/src/modelo/Pergunta.java
@@ -0,0 +1,64 @@
+package modelo;
+
+public class Pergunta {
+    private String a, b, c, d, certa;
+    private Integer id, nivel;
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+    public String getC() {
+        return c;
+    }
+
+    public void setC(String c) {
+        this.c = c;
+    }
+
+    public String getD() {
+        return d;
+    }
+
+    public void setD(String d) {
+        this.d = d;
+    }
+
+    public String getCerta() {
+        return certa;
+    }
+
+    public void setCerta(String certa) {
+        this.certa = certa;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(Integer nivel) {
+        this.nivel = nivel;
+    }
+    
+    
+}
---
+++ showdoMilhaoAndressa/src/showdomilhaoandressa/ShowdoMilhaoAndressa.java
@@ -0,0 +1,9 @@
+package showdomilhaoandressa;
+
+public class ShowdoMilhaoAndressa {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
